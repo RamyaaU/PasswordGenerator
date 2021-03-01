43e5862bda83acfad62754404b32e63a084c1de9
Ramya.U <ramyayadav2016@gmail.com>
2021-03-01 12:56:26 +0530

    [Ramya] Add . Added event object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  length = 0;
   includeLetters = false;
   includeNumbers  = false;
   includeSymbols = false;
   title = 'PasswordGenerator';
 
   password ='';
+
+  onChangeLength(value:string){
+    const parsedvalue = parseInt(value);
+
+    //Nan is a built in fucntion which says is not a number
+    if(!isNaN(parsedvalue)){
+      this.length = parsedvalue;
+    }
+  }
 
   onChangeUseLetters(){
     this.includeLetters = !this.includeLetters;
