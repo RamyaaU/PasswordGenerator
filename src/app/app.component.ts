a626b262b8c24b1852a7452f15a3a552c1e51619
Ramya.U <ramyayadav2016@gmail.com>
2021-03-01 12:24:03 +0530

    [Ramya] Add . Added on button click function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  includeLetters = false;
+  includeNumbers  = false;
+  includeSymbols = false;
   title = 'PasswordGenerator';
 
   password ='';
 
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+
+  onChangeUseSymbols(){
+    this.includeSymbols= !this.includeSymbols;
+  }
+
   onButtonClick(){
+    console.log(`About to generate a password with the following: 
+      Includes letters: ${this.includeLetters}
+      Includes numbers: ${this.includeNumbers}
+      Includes symbols: ${this.includeSymbols}
+      `);
     this.password = 'MY PASSWORD!!!';
   }
 
